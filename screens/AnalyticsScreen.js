912361a4206f40521033ed1ea99313fc6d3b3845
jasonymk97 <jasonymk97@gmail.com>
2024-05-29 11:34:11 +0800

    Provide income and expense option

--- screens/AnalyticsScreen.js
+++ screens/AnalyticsScreen.js
@@ -1,14 +1,15 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity, ScrollView} from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity, ScrollView } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import Layout from '../components/Layout';
 import transactionService from '../services/transactionService';
 import CustomPieChart from '../components/analytics/CustomPieChart';
 import CustomBarChart from '../components/analytics/CustomBarChart';
-import { categoryNames } from '../constants/globalConstants';
+import { categoryNames, transactionTypeSelectItems } from '../constants/globalConstants';
 import CustomLineChart from '../components/analytics/CustomLineChart';
 import { summarizePieChartData, summarizeTransactionsData } from '../utils/chartDataUtils';
+import RNPickerSelect from 'react-native-picker-select';
 
 export default function AnalyticsScreen() {
     const [isStartPickerVisible, setStartPickerVisibility] = useState(false);
@@ -19,9 +20,10 @@ export default function AnalyticsScreen() {
     const [refreshFlag, setRefreshFlag] = useState(false);
     const [transactionChartData, setTransactionChartData] = useState([]);
     const [pieChartData, setPieChartData] = useState([]);
+    const [type, setType] = useState('expense'); // Default type is expense
 
     const handleRefreshChart = useCallback(() => {
-      setRefreshFlag(prevFlag => !prevFlag); // Toggle the refresh flag
+        setRefreshFlag(prevFlag => !prevFlag); // Toggle the refresh flag
     }, []);
 
     const showStartPicker = () => {
@@ -81,32 +83,32 @@ export default function AnalyticsScreen() {
 
     useFocusEffect(
         useCallback(() => {
-          let timeoutId;
-    
-          // Set a timeout to delay the API call by 1 second
-          timeoutId = setTimeout(fetchTransactions, 1000);
-    
-          // Cleanup function to clear the timeout if the focus is lost before 1 second
-          return () => clearTimeout(timeoutId);
+            let timeoutId;
+
+            // Set a timeout to delay the API call by 1 second
+            timeoutId = setTimeout(fetchTransactions, 1000);
+
+            // Cleanup function to clear the timeout if the focus is lost before 1 second
+            return () => clearTimeout(timeoutId);
         }, [])
-      );
-    
-      const fetchTransactions = async () => {
+    );
+
+    const fetchTransactions = async () => {
         try {
-          const response = await transactionService.getTransactions();
-          console.log('api call');
-          if (response.isError) {
-            console.log('Error fetching expenses:', response.errorMessage);
-            return;
-          }
-          setTransactions(response.data);
+            const response = await transactionService.getTransactions();
+            console.log('api call');
+            if (response.isError) {
+                console.log('Error fetching Transactions:', response.errorMessage);
+                return;
+            }
+            setTransactions(response.data);
         } catch (error) {
-          console.error('Error fetching expenses:', error);
+            console.error('Error fetching Transactions:', error);
         }
-      };
+    };
 
 
-      useEffect(() => {
+    useEffect(() => {
         if (startDate && endDate && transactions.length > 0) {
             console.log('Filtering transactions');
 
@@ -115,7 +117,7 @@ export default function AnalyticsScreen() {
             const endTimestamp = endDate.getTime();
 
             const filteredTransactions = transactions.filter((transaction) => {
-                return transaction.date >= startTimestamp && transaction.date <= endTimestamp && transaction.type === 'expense';
+                return transaction.date >= startTimestamp && transaction.date <= endTimestamp && transaction.type === type;
             });
 
             console.log('Filtered transactions:', filteredTransactions);
@@ -123,29 +125,40 @@ export default function AnalyticsScreen() {
             const summarizedData = summarizeTransactionsData(filteredTransactions, startDate, endDate);
             console.log("🚀 ~ useEffect ~ summarizedData:", summarizedData)
             const generatedPieData = summarizePieChartData(filteredTransactions, categoryNames);
-            
+
             setTransactionChartData(summarizedData);
             setPieChartData(generatedPieData);
             // Force to refresh the chart
             handleRefreshChart();
         }
-    }, [startDate, endDate, transactions]);
- 
+    }, [startDate, endDate, transactions, type]);
+
     return (
         <Layout>
-            <View style={{backgroundColor: '#FFFFFF', borderRadius: 20, marginVertical: 20, borderWidth: 1}}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 20}}>
-                <View>
-                    <TouchableOpacity onPress={() => showStartPicker()}>
-                    {startDate && <Text style={styles.dateText}>&lt; {formatDate(startDate)} &gt;</Text>}
-                    </TouchableOpacity>
+            <View style={{ flexDirection: 'row', marginTop: 20, backgroundColor: '#FFFFFF', padding: 10, borderRadius: 20, justifyContent: 'center'}}>
+                <Text style={styles.typeText} >Type: </Text>
+                <View style={{justifyContent: 'center'}}>
+                    <RNPickerSelect
+                        style={{}}
+                        value={type}
+                        onValueChange={(itemValue) => setType(itemValue)}
+                        items={transactionTypeSelectItems}>
+                    </RNPickerSelect>
                 </View>
-                <Text style={styles.dateText}>to</Text>
-                <View>
-                    <TouchableOpacity onPress={() => showEndPicker()}>
-                    {endDate && <Text style={styles.dateText}>&lt; {formatDate(endDate)} &gt;</Text>}
-                    </TouchableOpacity>
-                </View>
+            </View>
+            <View style={{ backgroundColor: '#FFFFFF', borderRadius: 20, marginVertical: 20 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10, marginHorizontal: 50}}>
+                    <View>
+                        <TouchableOpacity onPress={() => showStartPicker()}>
+                            {startDate && <Text style={styles.dateText}> {formatDate(startDate)}</Text>}
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.dateText}>to</Text>
+                    <View>
+                        <TouchableOpacity onPress={() => showEndPicker()}>
+                            {endDate && <Text style={styles.dateText}>{formatDate(endDate)}</Text>}
+                        </TouchableOpacity>
+                    </View>
                 </View>
 
                 <DateTimePickerModal
@@ -162,15 +175,15 @@ export default function AnalyticsScreen() {
                     onCancel={hideEndPicker}
                 />
             </View>
-            <View style={{flexDirection: 'row', justifyContent: 'space-around', marginBottom: 20}}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 20 }}>
                 <Button title="Last Month" onPress={setLastMonth} />
                 <Button title="Last Week" onPress={setLastWeek} />
                 <Button title="Last Year" onPress={setLastYear} />
             </View>
             <ScrollView>
-            <CustomBarChart key={refreshFlag.toString()} data={transactionChartData}/>
-            <CustomLineChart data={transactionChartData} />
-            <CustomPieChart data={pieChartData} />
+                <CustomBarChart key={refreshFlag.toString()} data={transactionChartData} />
+                <CustomLineChart data={transactionChartData} />
+                <CustomPieChart data={pieChartData} />
             </ScrollView>
         </Layout>
     );
@@ -178,8 +191,10 @@ export default function AnalyticsScreen() {
 
 const styles = StyleSheet.create({
     dateText: {
-        marginTop: 10,
-        fontSize: 18,
+        fontWeight: '700',
+        color: '#000000'
+    },
+    typeText: {
         fontWeight: '700',
         color: '#000000'
     },
